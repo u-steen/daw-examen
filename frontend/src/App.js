d00f6f3990bc96663df1a48962fc4247ec44eb0a
Iustin Esanu <iustin.esanu7@gmail.com>
2024-01-24 22:06:07 +0200

    init 2

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={async () => {
+          var response = await fetch("/api/test");
+          var data = await response.json();
+          console.log(data);
+        }}>
+          Fetch
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
